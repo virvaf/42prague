bd39ab8b8df39fef2cf6a5cf178d2dc47c721f20
V-Software <v-software@seznam.cz>
2026-02-24 21:31:17 +0100

    Added get_next_line project files

---
+++ Milestone-1/get_next_line/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: virva <virva@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 21:29:41 by virva             #+#    #+#             */
+/*   Updated: 2026/02/24 21:29:43 by virva            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+/*   Required libraries   */
+# include <unistd.h>
+# include <stdlib.h>
+
+/*   get_next_line prototypes   */
+
+
+/*   Utils prototypes   */
+
+
+#endif
