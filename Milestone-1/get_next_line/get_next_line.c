6530f035739402035ddca999129abc30bfe936d9
V-Software <v-software@seznam.cz>
2026-05-10 18:47:53 +0200

    something

--- Milestone-1/get_next_line/get_next_line.c
+++ Milestone-1/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fvirva <fvirva@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 21:30:00 by virva             #+#    #+#             */
-/*   Updated: 2026/04/25 23:13:09 by fvirva           ###   ########.fr       */
+/*   Updated: 2026/05/10 18:33:13 by fvirva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 char *get_next_line(int fd)
 {
-    if(!(fd > 2))
-    {
+    if(fd < 0 || BUFFER_SIZE <= 0)
         return NULL;
-    }
+
+    static char* stash;
+    char buffer[BUFFER_SIZE + 1];
+
+    stash = (char *)malloc(ft_strlen(stash) + BUFFER_SIZE + 1);
 }
